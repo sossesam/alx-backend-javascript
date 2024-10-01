9619d96a965a17b158685d715ad5feaae5598cf1
sanmi <sossesam@gmail.com>
2024-10-01 06:59:43 +0100

    all

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -6,12 +6,10 @@ function getCurrentYear() {
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   const date = getCurrentYear();
   const budget = {
-  [`income-${[date]}`] : income,
-  [`gdp-${[date]}`] : gdp,
-  [`capita-${[date]}`] : capita,
+    [`income-${[date]}`]: income,
+    [`gdp-${[date]}`]: gdp,
+    [`capita-${[date]}`]: capita,
   };
-
-  
 
   return budget;
 }

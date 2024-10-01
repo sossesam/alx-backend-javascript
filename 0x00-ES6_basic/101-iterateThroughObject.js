94e7fbcc51337a1fb9987f08106544fe331a5075
sanmi <sossesam@gmail.com>
2024-10-01 12:11:13 +0100

    all

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -3,5 +3,5 @@ export default function iterateThroughObject(reportWithIterator) {
   for (const empName of reportWithIterator) {
     newstring.push(empName);
   }
-  return (newstring.toString().replaceAll(',', '|'));
+  return reportWithIterator = newstring.toString().replaceAll(',', '|');
 }

04eb83b8b965542075743078091bb87bb6966253
sanmi <sossesam@gmail.com>
2024-10-01 06:54:52 +0100

    all

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
- 
-  for (let idx of array) {
-    const index = array.indexOf(idx);
-    array[index] = appendString + idx;
+  const newArray = [];
+  for (const idx of array) {
+    newArray.push(appendString + idx);
   }
 
-  return array;
+  return newArray;
 }

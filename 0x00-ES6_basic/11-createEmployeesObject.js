1d933921d38df2d8d2635d6b1290b67dce9297e7
sanmi <sossesam@gmail.com>
2024-10-01 08:04:26 +0100

    all

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,3 +1,9 @@
 export default function createEmployeesObject(departmentName, employees) {
-  return ({ departmentName: employees });
+    let obj = {}
+    let anArray = []
+    obj[`${departmentName}`]= anArray
+    for(const emp of employees){
+        anArray.push(emp)
+    }
+    return obj;
 }

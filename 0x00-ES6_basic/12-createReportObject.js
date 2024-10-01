f5b684603b9cc6bf386c9b1dae6d0af63ac48118
sanmi <sossesam@gmail.com>
2024-10-01 10:20:50 +0100

    all

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,11 +1,10 @@
 export default function createReportObject(employeeList) {
- 
- let allEmployees = {
-    funcname:"name",
-    func: function (){console.log(this.funcname)}
- }
- return allEmployees;
+  const theObject = {};
+  const emplist = {};
+  for (const [key, value] of Object.entries(employeeList)) {
+    emplist[`${key}`] = value;
+  }
+  theObject.allEmployees = emplist;
+  theObject.getNumberOfDepartments = () => Object.keys(theObject).length;
+  return theObject;
 }
-
-let call = createReportObject({ Software: [ 'Bob', 'Sylvie' ] })
-call.func()

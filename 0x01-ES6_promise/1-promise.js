ea4f98eeecaef0dd9bdb3fe2e6035d01312b747c
sanmi <sossesam@gmail.com>
2024-10-02 23:08:52 +0100

    all

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,6 +1,6 @@
 export default function getResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (success === true) {
+    if (success) {
       resolve({ status: 200, body: 'photo-profile-1' });
     } else {
       reject(new Error('The fake API is not working currently'));

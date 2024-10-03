b01273f6501c3316c88669acd42ea471cd4821b1
sanmi <sossesam@gmail.com>
2024-10-03 06:11:37 +0100

    all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -5,4 +5,5 @@ export default function handleProfileSignup() {
   const uploadPhotoPromise = uploadPhoto();
   Promise.all([createUserPromise, uploadPhotoPromise])
   .then((values)=>{console.log(values[1].body, values[0].firstName, values[0].lastName)})
+  .catch(console.error("Signup system offline"))
 }

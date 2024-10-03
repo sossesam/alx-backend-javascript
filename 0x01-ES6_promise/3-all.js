5fbbc6fb5a6804e7cd31cce8ab518b1e88b67c2c
sanmi <sossesam@gmail.com>
2024-10-03 06:04:37 +0100

    all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,13 +3,5 @@ import { createUser, uploadPhoto } from './utils';
 export default function handleProfileSignup() {
   const createUserPromise = createUser();
   const uploadPhotoPromise = uploadPhoto();
-  const display = [];
-  uploadPhotoPromise.then((value) => {
-    display.push(value.body);
-    return createUserPromise;
-  }).then((value) => {
-    display.push(value.firstName);
-    display.push(value.lastName);
-    console.log(display.join(' '));
-  });
+  Promise.all([createUserPromise, uploadPhotoPromise]).then((values)=>{console.log(values)})
 }

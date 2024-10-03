b475d0a0415a019f774b5c04837b23981390a4b2
sanmi <sossesam@gmail.com>
2024-10-03 09:20:11 +0100

    all

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -6,11 +6,11 @@ export default function handleResponseFromAPI(promise) {
           status: 200,
           body: 'success',
         });
-        console.log('Got a response from the API');
+    
       }
     } catch (err) {
       reject(new Error());
-      console.log('Got a response from the API');
+     
     } finally { console.log('Got a response from the API'); }
   });
 }

9d1151564aacdf3a293ceafb74a04c137e8d314a
sanmi <sossesam@gmail.com>
2024-10-03 14:22:35 +0100

    all

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,6 +3,12 @@ import signUpUser from './4-user-promise';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => { return values})
-    .catch(Error);
+    .then((values) => {
+      const arr = [];
+      values.forEach((element) => {
+        arr.push({ status: 200, value: element });
+      });
+      return arr;
+    })
+    .catch((err) => ({ status: 100, value: err }));
 }

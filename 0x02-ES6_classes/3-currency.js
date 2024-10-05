50883069f10a431ec3f67779b4e06cf3b7c8ed8e
sanmi <sossesam@gmail.com>
2024-10-05 11:03:58 +0100

    all

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,28 @@
+export default class Currency {
+/* eslint-disable no-underscore-dangle */
+  constructor(code, name) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set code(code) {
+    this._code = code;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
+/* eslint-enable no-underscore-dangle */

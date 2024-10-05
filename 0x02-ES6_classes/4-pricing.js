ca7b1e762d72946e52fa8500f85265fd3cfd1075
sanmi <sossesam@gmail.com>
2024-10-05 12:42:17 +0100

    all

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-underscore-dangle */
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this.currency.name} (${this.currency.code})`;
+  }
+}
+/* eslint-enable no-underscore-dangle */

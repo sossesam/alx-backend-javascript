ff8f0cc4e6b8581ba8f8c815be98162f654484dc
sanmi <sossesam@gmail.com>
2024-10-12 17:50:42 +0100

    all

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+export default function guardrail(mathFunction) {
+    const queue = [];
+  
+    try {
+      const result = mathFunction();
+      queue.push(result);
+    } catch (error) {
+      queue.push(error.toString());
+    } finally {
+      queue.push('Guardrail was processed');
+    }
+  
+    return queue;
+  }

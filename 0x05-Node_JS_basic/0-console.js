61a6f1ec29c424d89b6257225e659ac534e9f9f4
sanmi <sossesam@gmail.com>
2024-11-25 13:58:46 +0100

    all

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,6 @@
+module.exports = function displayMessage(str_argument){
+   process.stdout.write(str_argument + "\n")
+  }
+
+
+
---
+++ 0x05-Node_JS_basic/0-main.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+const displayMessage = require('./0-console');
+
+displayMessage("Hello NodeJS!");
+

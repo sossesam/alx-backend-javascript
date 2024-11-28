ddec99142ece133e56712419540e47f3874f71f0
sanmi <sossesam@gmail.com>
2024-11-28 22:43:15 +0100

    all

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,6 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.write('Hello Holberton School!'); //write a response to the client
+    res.end(); //end the response
+  }).listen(1245);

3fff6e474daec6fc26cef013cd5c966ff3ec3821
sanmi <sossesam@gmail.com>
2024-11-28 22:45:56 +0100

    all

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,6 +1,8 @@
 var http = require('http');
 
-http.createServer(function (req, res) {
+let app = http.createServer(function (req, res) {
     res.write('Hello Holberton School!'); //write a response to the client
     res.end(); //end the response
   }).listen(1245);
+
+module.exports = app

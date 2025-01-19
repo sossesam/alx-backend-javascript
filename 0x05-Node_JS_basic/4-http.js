0eb9a52579d6d27468101280a1bed7bd12665b0f
sanmi <sossesam@gmail.com>
2025-01-19 18:36:11 +0100

    all

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,7 +3,7 @@ const http = require('http');
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('Hello ALX!');
+  res.end('Hello Holberton School!');
 }).listen(1245);
 
 module.exports = app;

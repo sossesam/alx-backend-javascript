cac9a3f3f693a9755fd2975699203192709703fa
sanmi <sossesam@gmail.com>
2025-01-19 18:28:33 +0100

    all

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  res.writeHead(200);  
+  res.writeHead(200, { 'Content-Type': 'text/plain' });  // write a response to the client
   res.write('Hello ALX!');
   res.end(); // end the response
 }).listen(1245);
